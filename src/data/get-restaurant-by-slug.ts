0e0829ddc6317f4663e338070856fa580f633b3d
Leonardo Braga <programer_frontbraga@outlook.com>
2025-04-08 20:26:04 -0300

    feat: add initial restaurant page and menu page

---
+++ src/data/get-restaurant-by-slug.ts
@@ -0,0 +1,7 @@
+import { db } from '@/lib/prisma';
+
+export async function getRestaurantBySlug(slug: string) {
+	const restaurant = await db.restaurant.findUnique({ where: { slug } });
+
+	return restaurant;
+}
